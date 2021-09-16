a57c9a53a44e544c74168d914d7300a1d1936d52
elmerbool <35117496+elmerbool@users.noreply.github.com>
2021-09-17 07:55:01 +1000

    Initial commit

---
+++ src/main/ItemData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ei8.Data.ExternalReference.Common
+{
+    public class ItemData
+    {
+        public string Id { get; set; }
+
+        public string Url { get; set; }
+
+        public int Version { get; set; }
+    }
+}
